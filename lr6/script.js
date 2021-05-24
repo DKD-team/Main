90bdd196e3e947f517bf83d7486d9c9eca441b38
DKD-team <79047173+DKD-team@users.noreply.github.com>
2021-05-24 15:34:59 +0300

    Add files via upload

---
+++ lr6/script.js
@@ -0,0 +1,510 @@
+var adList = [
+
+ {
+
+   id: '1',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+    {id: '2',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+    {id: '3',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+    {id: '4',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '5',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+  {id: '6',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+  {id: '7',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '8',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+  {id: '9',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '10',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '11',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '12',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '13',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '14',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+  {id: '15',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+  {id: '16',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+  {id: '17',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '18',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+ { id: '19',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ },
+  {id: '20',
+
+   description: 'Скидка на стулья - до 15%',
+
+   createdAt: new Date('2021-03-15T23:00:00'),
+
+   link: 'https://coolchairs.com',
+
+   vendor: 'Chair Service',
+
+   photoLink: 'https://images.app.goo.gl/dgAFyP2cEduzkJUP9',
+
+   validUntil: new Date('2021-06-01T23:00:00'),
+
+   discount: '15%',
+
+   hashTags: ['furniture', 'chairs']
+
+ }
+];
+var adListFunc = (function(){
+    
+    function getAds(skip, top, filterConfig){
+        if(skip === undefined){
+            skip = 0;
+        }
+        if(top === undefined){
+            top = 10;
+        }
+        var adListTmp = adList.slice();
+                    adListTmp.sort(function(a,b){
+                return a > b;
+            });
+        
+        if(!(filterConfig === undefined)){
+            return adListTmp;
+        }
+        else{
+            var to_remove = [];
+            for(var i = 0; i < adListTmp.size;i++){
+                var flag = true;
+                if("createdAt" in filterConfig){
+                    if(adListTmp[i].createdAt !== filterConfig.createdAt){
+                        flag = false;
+                    }
+                }
+                if("vendor" in filterConfig){
+                    if(adListTmp[i].vendor !== filterConfig.vendor){
+                        flag = false;
+                    }
+                }
+                if(!flag){
+                    to_remove.push(i);
+                }
+            }
+            for(var i = to_remove.size - 1;i >= 0;i--){
+                adListTmp.splice(to_remove[i], 1)
+            }
+            
+            
+        }
+    }
+    function getAd(id){
+        for(var i = 0; i < adList.size;i++){
+            if(adList[i].id === id)
+                return adList[i];
+        }
+    }
+    function validateAd(adItem){
+        if(!("id" in adItem))
+            return false;
+        if(!("description" in adItem))
+            return false;
+        if(!("createdAt" in adItem))
+            return false;
+        if(!("link" in adItem))
+            return false;
+        if(!("vendor" in adItem))
+            return false;
+        if(!("photoLink" in adItem))
+            return false;
+        if(!("hashTags" in adItem))
+            return false;
+        if(!("discount" in adItem))
+            return false;
+        if(!("validUntil" in adItem))
+            return false;
+        if(!("rating" in adItem))
+            return false;
+        if(!("reviews" in adItem))
+            return false;
+        
+        return true;
+    }
+    function addAd(adItem){
+        if(!validateAd(adItem))
+            return false;
+        adList.push(adItem);
+        return true;
+    }
+    function editAd(id, adItem){
+        if(adItem === undefined){
+            return false;
+        }
+        for(var i = 0 ; i < adItem;i++){
+            if(adList[i].id === id){
+                if("description" in adItem){
+                    adList[i].description = adItem.description;
+                }
+                if("link" in adItem){
+                    adList[i].link = adItem.link;
+                }
+                if("photoLink" in adItem){
+                    adList[i].photoLink = adItem.photoLink;
+                }
+                if("hashTags" in adItem){
+                    adList[i].hashTags = adItem.hashTags;
+                }
+                if("discount" in adItem){
+                    adList[i].discount = adItem.discount;
+                }
+                if("validUntil" in adItem){
+                    adList[i].validUntil = adItem.validUntil;
+                }
+                if("rating" in adItem){
+                    adList[i].rating = adItem.rating;
+                }
+                if("reviews" in adItem){
+                    adList[i].reviews = adItem.reviews;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    function removeAd(id){
+        for(var i = 0; i < adList.size;i++){
+            if(adList[i] === id){
+                adList.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+})();
